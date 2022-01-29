7b352bfa03ae5fb76d7195c27ebb8956c2f93127
kostusta <kostiusta@gmail.com>
2022-01-29 07:38:25 +0000

    Deploying to gh-pages from @ kostusta/filmoteka-team-project-js@99a9ca5da54254da1cdff50d575d1e084f87c55c 🚀

--- src.61f87c08.js
+++ src.61f87c08.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/filmoteka-team-project-js/src.5bcc6ee2.js.map
+//# sourceMappingURL=/filmoteka-team-project-js/src.61f87c08.js.map
