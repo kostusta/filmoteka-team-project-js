f286008a663e47bdd0dc6bbaace9748e0e187f90
Stanislav Kostiuchenko <kostiusta@gmail.com>
2022-02-04 01:55:33 +0200

    Revert "My library buttons handlers"

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ import { getColection } from './js/galery';
 import {} from './js/film-modal';
 
 import {} from './js/team-modal';
+
 getColection();
--- src/js/handlebars.js
+++
@@ -1,5 +0,0 @@
-Handlebars.registerHelper('release-year', function (release_date) {
-  const releaseDate = release_date;
-  const releaseYear = releaseDate.slice(0, 3);
-  return releaseYear;
-});
