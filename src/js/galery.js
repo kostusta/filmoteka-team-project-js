3ca7978e01c266edf8fe02a804149259fdb8d3bb
Andriy Dzyunyak <andriy.dzyunyak@gmail.com>
2022-02-04 23:07:09 +0200

    added pagination and their styles

--- src/js/galery.js
+++ src/js/galery.js
@@ -1,5 +1,5 @@
 import filmCard from '../templates/film-card.hbs';
-import { fetchMovies } from './api';
+//import { fetchMovies } from './api';
 
 const refs = {
   gallery: document.querySelector('.library__list'),
@@ -7,13 +7,15 @@ const refs = {
   closeModal: document.querySelector('.close-button'),
 };
 
-export function getColection() {
-  fetchMovies().then(data => {
-    renderGalery(data);
-  });
-}
+// export function getColection() {
+//   fetchMovies().then(data => {
+//     renderGalery(data);
+//   });
+// }
 
-function renderGalery({ results }) {
+export function renderGalery({ results }) {
   const markup = results.map(filmCard);
+  refs.gallery.innerHTML = '';
   refs.gallery.insertAdjacentHTML('beforeend', markup.join(''));
+  console.log('Зарендерили', results);
 }
