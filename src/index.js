abf8ceaa7b12deec2876beec993ceb5f7a63f549
Andriy Dzyunyak <andriy.dzyunyak@gmail.com>
2022-02-05 23:45:01 +0200

    added ontop button

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './sass/main.scss';
 import './js/header';
 import './js/header-buttons-handlers';
 import './js/pagination';
+import './js/ontop-btn';
 import './js/film-modal';
 import './js/team-modal';
 import './js/auth';
